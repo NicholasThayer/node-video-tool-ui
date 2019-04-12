6a232bf26c7ec805fe1ac1ec5b215fbc495b43bb
NicholasThayer <thayer.nicholas@gmail.com>
2019-04-12 19:25:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  preventDefault = ( event ) => {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -20,6 +26,22 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <form
+            class="box upload-box"
+            method="post"
+            action=""
+            enctype="multipart/form-data"
+          >
+          <div class="box__input">
+            <input class="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
+            <label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
+            <button class="box__button" type="submit">Upload</button>
+          </div>
+          <div class="box__uploading">Uploading&hellip;</div>
+          <div class="box__success">Done!</div>
+          <div class="box__error">Error! <span></span>.</div>
+        </form>
+
       </div>
     );
   }
