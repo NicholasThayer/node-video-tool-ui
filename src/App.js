126151a8976d5d09e376adeecf49af3ff6792105
NicholasThayer <thayer.nicholas@gmail.com>
2019-04-14 17:41:24 -0400

    added react-file-drop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import FileDrop from 'react-file-drop';
 import logo from './logo.svg';
 import './App.css';
+import './file-drop.css';
 
 class App extends Component {
-  preventDefault = ( event ) => {
-    event.preventDefault();
-    event.stopPropagation();
+  handleDrop = (files, evt) => {
+    console.log('handleDrop', files, evt);
   }
 
   render() {
@@ -17,31 +18,12 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
-        <form
-            class="box upload-box"
-            method="post"
-            action=""
-            enctype="multipart/form-data"
-          >
-          <div class="box__input">
-            <input class="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
-            <label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
-            <button class="box__button" type="submit">Upload</button>
-          </div>
-          <div class="box__uploading">Uploading&hellip;</div>
-          <div class="box__success">Done!</div>
-          <div class="box__error">Error! <span></span>.</div>
-        </form>
-
+        <div className="file-drop-box">
+          <FileDrop onDrop={this.handleDrop}>
+            drop files!
+          </FileDrop>
+        </div>
       </div>
     );
   }
